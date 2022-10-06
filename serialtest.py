2c5cb052ea7fd55d16a7dfe38639244b92b8a916
ganeshdharma <91176736+ganeshdharma@users.noreply.github.com>
2022-10-06 15:10:52 +0530

    Update serialtest.py

--- serialtest.py
+++ serialtest.py
@@ -4,7 +4,7 @@ import time
 
 class serialtest(object):
     def test(self, a):
-        ser = serial.Serial("COM5")  # Open named port
+        ser = serial.Serial("COM4")  # Open named port
         ser.baudrate = 9600  # Set baud rate to 9600
         # Read ten characters from serial port to data
         ser.write(b'hello world')
